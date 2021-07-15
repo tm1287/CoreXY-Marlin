6fe1e7cf1c5bbe73e6a5b7e485b350beb454effd
Tejas <tejas@MacBook-Pro.fios-router.home>
2021-07-15 19:17:55 -0400

    Adjust probe-nozzle offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1159,7 +1159,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 10, 10, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 30, 18, 0 } //TM
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
